494f8667bfdbd5a56152bf94b49696d66615c1fd
Dharanee2025 <dharanee1417@gmail.com>
2025-09-24 15:05:30 +0530

    Second Commit

---
+++ src/sam/org/Test.java
@@ -0,0 +1,26 @@
+package sam.org;
+
+public class Test
+{
+public static void main(String[] args)
+{
+try
+{
+System.out.println("5");
+int data = 5 / 0;
+}
+catch(ArithmeticException e)
+{
+System.out.println("2");
+System.exit(5658);
+}
+finally
+{
+System.out.println("3");
+}
+System.out.println("4");
+}
+}
+
+
+	 
